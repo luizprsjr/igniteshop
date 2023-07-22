80fc382335471439e51f0f991142f2da9b61a6d4
Luiz Pires <lpiresjunior@gmail.com>
2023-07-22 12:08:45 -0300

    feat: create app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
+import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
