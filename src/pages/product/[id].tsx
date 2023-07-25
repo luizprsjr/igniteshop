9b923cb4b6262940e9c3fbd95fee9bea08b5e31a
Luiz Pires <lpiresjunior@gmail.com>
2023-07-24 22:24:03 -0300

    feat: create product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
 import { useRouter } from 'next/router'
 
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer />
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          facere similique rem sunt assumenda perferendis, voluptatum, molestias
+          error animi, earum reiciendis pariatur porro tempora maiores rerum
+          eveniet? Dolores, et accusantium.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
